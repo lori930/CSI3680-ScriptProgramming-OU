0df839694eeb94c02461395b6b71455fc3d3288f
Lori Xu <xly0213@gmail.com>
2025-10-09 11:34:45 -0400

    import update

--- Functions and Classes/main.py
+++ Functions and Classes/main.py
@@ -1,4 +1,9 @@
-import math_utils
+# import math_utils
 
-print(math_utils.square(4))
-print(math_utils.cube(3))
+# print(math_utils.square(4))
+# print(math_utils.cube(3))
+
+from math_utils import *
+
+print(square(4))
+print(cube(3))
